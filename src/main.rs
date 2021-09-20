871a6da2257a6a14e054935341a14c954a50366b
steve <ssosik@akamai.com>
2021-09-20 15:55:45 -0400

    Adding in structopt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,54 @@
-use clap::{App, Arg};
 use color_eyre::Report;
 use glob::{glob, Paths};
 use meilisearch_cli::Document;
 use std::path::Path;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "meilisearch-cli",
+    about = "CLI interface to Meilisearch to storing and retrieving Zettelkasten-style notes",
+    author = "Steve <steve@little-fluffy.cloud>"
+)]
+struct Opt {
+    /// switch on verbosity
+    #[structopt(short)]
+    verbose: bool,
+
+    #[structopt(subcommand)]
+    import: MdImportCmd,
+    // /// Activate debug mode
+    //// short and long flags (-d, --debug) will be deduced from the field's name
+    //#[structopt(short, long)]
+    //debug: bool,
+
+    ///// Set speed
+    //// we don't want to name it "speed", need to look smart
+    //#[structopt(short = "v", long = "velocity", default_value = "42")]
+    //speed: f64,
+
+    ///// Input file
+    //#[structopt(parse(from_os_str))]
+    //input: PathBuf,
+
+    ///// Output file, stdout if not present
+    //#[structopt(parse(from_os_str))]
+    //output: Option<PathBuf>,
+
+    ///// Where to write the output: to `stdout` or `file`
+    //#[structopt(short)]
+    //out_type: String,
+
+    ///// File name: only required when `out-type` is set to `file`
+    //#[structopt(name = "FILE", required_if("out-type", "file"))]
+    //file_name: Option<String>,
+}
+
+#[derive(Debug, StructOpt)]
+enum MdImportCmd {
+    /// Unexpanded path and glob pattern
+    Import { globpath: String },
+}
 
 pub fn glob_files(source: &str, verbosity: i8) -> Result<Paths, Box<dyn std::error::Error>> {
     let glob_path = Path::new(&source);
@@ -27,52 +73,35 @@ fn setup() -> Result<(), Report> {
 fn main() -> Result<(), Report> {
     setup()?;
 
-    let cli = App::new("meilisearch-importer")
-        .version("1.0")
-        .author("Steve <steve@little-fluffy.cloud>")
-        .about("Read my vimdiary markdown files and import them into local Meilisearch")
-        .arg(
-            Arg::with_name("v")
-                .short("v")
-                .multiple(true)
-                .help("Sets the level of verbosity"),
-        )
-        .arg(
-            Arg::with_name("globpath") // And their own arguments
-                .help("the files to add")
-                .required(true),
-        )
-        .get_matches();
-
+    let cli = Opt::clap().get_matches();
     let verbosity = cli.occurrences_of("v");
 
-    let client = reqwest::blocking::Client::new();
+    if let Some(cli) = cli.subcommand_matches("import") {
+        let client = reqwest::blocking::Client::new();
 
-    // Read the markdown files and post them to local Meilisearch
-    for entry in glob_files(
-        cli.value_of("globpath").unwrap(),
-        cli.occurrences_of("v") as i8,
-    )
-    .expect("Failed to read glob pattern")
-    {
-        match entry {
-            // TODO convert this to iterator style using map/filter
-            Ok(path) => {
-                if let Ok(mdfm_doc) = markdown_fm_doc::parse_file(&path) {
-                    let doc: Vec<Document> = vec![mdfm_doc.into()];
-                    let res = client
-                        .post("http://127.0.0.1:7700/indexes/notes/documents")
-                        .body(serde_json::to_string(&doc).unwrap())
-                        .send()?;
-                    if verbosity > 0 {
-                        println!("✅ {:?}", res,);
+        // Read the markdown files and post them to local Meilisearch
+        for entry in glob_files(cli.value_of("globpath").unwrap(), verbosity as i8)
+            .expect("Failed to read glob pattern")
+        {
+            match entry {
+                // TODO convert this to iterator style using map/filter
+                Ok(path) => {
+                    if let Ok(mdfm_doc) = markdown_fm_doc::parse_file(&path) {
+                        let doc: Vec<Document> = vec![mdfm_doc.into()];
+                        let res = client
+                            .post("http://127.0.0.1:7700/indexes/notes/documents")
+                            .body(serde_json::to_string(&doc).unwrap())
+                            .send()?;
+                        if verbosity > 0 {
+                            println!("✅ {:?}", res,);
+                        }
+                    } else {
+                        eprintln!("❌ Failed to load file {}", path.display());
                     }
-                } else {
-                    eprintln!("❌ Failed to load file {}", path.display());
                 }
-            }
 
-            Err(e) => eprintln!("❌ {:?}", e),
+                Err(e) => eprintln!("❌ {:?}", e),
+            }
         }
     }
 
