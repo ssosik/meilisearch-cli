6aa2241a173203d66820825adae344163ea2c441
steve <ssosik@akamai.com>
2021-09-20 16:10:55 -0400

    Structopt cleanups

--- src/main.rs
+++ src/main.rs
@@ -16,41 +16,16 @@ struct Opt {
     #[structopt(short)]
     verbose: bool,
 
-    #[structopt(short,long,default_value="http://127.0.0.1:7700")]
+    #[structopt(short, long, default_value = "http://127.0.0.1:7700")]
     host: String,
 
     #[structopt(subcommand)]
     import: Subcommands,
-    // /// Activate debug mode
-    //// short and long flags (-d, --debug) will be deduced from the field's name
-    //#[structopt(short, long)]
-    //debug: bool,
-
-    ///// Set speed
-    //// we don't want to name it "speed", need to look smart
-    //#[structopt(short = "v", long = "velocity", default_value = "42")]
-    //speed: f64,
-
-    ///// Input file
-    //#[structopt(parse(from_os_str))]
-    //input: PathBuf,
-
-    ///// Output file, stdout if not present
-    //#[structopt(parse(from_os_str))]
-    //output: Option<PathBuf>,
-
-    ///// Where to write the output: to `stdout` or `file`
-    //#[structopt(short)]
-    //out_type: String,
-
-    ///// File name: only required when `out-type` is set to `file`
-    //#[structopt(name = "FILE", required_if("out-type", "file"))]
-    //file_name: Option<String>,
 }
 
 #[derive(Debug, StructOpt)]
 enum Subcommands {
-    /// Unexpanded path and glob pattern
+    /// Import frontmatter+markdown formatted files matching the unexpanded glob pattern
     Import { globpath: String },
 }
 
